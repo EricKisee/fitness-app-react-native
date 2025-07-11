e034e5149742ae10822b5c554ac821d92ae09e03
EricKisee <erickiseemulwa@gmail.com>
2025-07-11 18:41:21 +0300

    Init -  45.00

---
+++ src/app/_layout.tsx
@@ -0,0 +1,12 @@
+import "../global.css";
+import { Slot, Stack, Tabs } from "expo-router";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { ClerkProvider } from "@clerk/clerk-expo";
+
+export default function Layout() {
+  return (
+    <ClerkProvider>
+      <Slot />
+    </ClerkProvider>
+  );
+}
